fbeebdf45b2f0cc3cdbffbe80cb4ca31bb884e5a
Jake LaCombe <jake.lacombe2@gmail.com>
2023-04-16 15:08:06 -0500

    Initialize Three.js

---
+++ src/app/render/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+import { GameScene } from '@/scenes/scene'
+import styles from '@/app/page.module.css'
+
+export default function Render() {
+  const canvas = useRef<HTMLCanvasElement | null>(null);
+  const scene = useRef<GameScene | null>();
+
+  useEffect(() => {
+    if (canvas.current && !scene.current) {
+      scene.current = new GameScene(canvas.current);
+      scene.current.animate();
+    }
+  })
+
+  return (
+    <main className={styles.main}>
+      <canvas ref={canvas} ></canvas>
+    </main>
+  )
+}
